d072fd1c747f0b6f8ea6ffa84248c69ee106d10d
anasofiperezz <anasofi.perez.sanchez@gmail.com>
2024-11-05 12:29:44 -0600

    Cambios tinaco

--- js/resultados.js
+++ js/resultados.js
@@ -1,25 +1,22 @@
-// Datos fijos basados en los datos proporcionados
-const datosFijos = {
-    precipAnual: "850 mm",
-};
-
 // Función para obtener el material del tanque según capacidad
 function obtenerMaterialRecomendado(capacidad) {
-    capacidad = Math.round(capacidad); // Redondear para evitar decimales en comparación
+    capacidad = Math.round(capacidad);
     if (capacidad === 500) return "Polietileno de alta densidad";
     if (capacidad === 750 || capacidad === 5000) return "Polietileno reforzado";
     if (capacidad === 1100 || capacidad === 1200) return "Polietileno de alta densidad";
+    if (capacidad === 2500) return "Polietileno de alta resistencia";
     return "Consultar material";
 }
 
 // Función para obtener el precio del tanque según su capacidad
 function obtenerPrecioTanque(capacidad) {
-    capacidad = Math.round(capacidad); // Redondear para evitar decimales en comparación
+    capacidad = Math.round(capacidad);
     let precio;
     if (capacidad === 500) precio = 2300;
     else if (capacidad === 750) precio = 3000;
     else if (capacidad === 1100) precio = 3600;
     else if (capacidad === 1200) precio = 3500;
+    else if (capacidad === 2500) precio = 10000;
     else if (capacidad === 5000) precio = 13000;
     else return "Precio a consultar";
     
@@ -32,17 +29,19 @@ function obtenerTuberiaRecomendada(capacidad) {
     if (capacidad === 500) return "1/2 Pulgada";
     if (capacidad === 750) return "3/4 Pulgada";
     if (capacidad === 1100 || capacidad === 1200) return "1 Pulgada";
+    if (capacidad === 2500) return "1 1/2 Pulgadas";
     if (capacidad === 5000) return "2 Pulgadas";
     return "Medida a consultar";
 }
 
 // Función para obtener el precio total de la tubería según la capacidad del tanque y la cantidad
 function obtenerPrecioTuberia(capacidad, cantidad) {
-    capacidad = Math.round(capacidad); // Redondear para evitar decimales en comparación
+    capacidad = Math.round(capacidad);
     let precioPorUnidad;
     if (capacidad === 500) precioPorUnidad = 15;
     else if (capacidad === 750) precioPorUnidad = 22;
     else if (capacidad === 1100 || capacidad === 1200) precioPorUnidad = 32.67;
+    else if (capacidad === 2500) precioPorUnidad = 53; 
     else if (capacidad === 5000) precioPorUnidad = 87.67;
     else return "Precio no disponible";
     
@@ -56,7 +55,7 @@ function obtenerAlturaTinaco(capacidad) {
     if (capacidad === 500) return 1.1;
     if (capacidad === 750) return 1.2;
     if (capacidad === 1100) return 1.4;
-    if (capacidad === 1200) return 1.5;
+    if (capacidad === 1200 || capacidad === 2500) return 1.5;
     if (capacidad === 5000) return 2.5;
     return "Altura del tinaco no disponible";
 }
@@ -87,25 +86,39 @@ function obtenerCodos(niveles) {
     return "Número de niveles fuera del rango permitido";
 }
 
+// Función para obtener el tinaco a utilizar
+function obtenerTinaco(litros) {
+    litros = Math.round(litros);
+    if (litros <= 600) return 500;
+    if (litros >= 601 && litros <= 800) return 750;
+    if (litros >= 801 && litros <= 1150) return 1100;
+    if (litros >= 1151 && litros <= 2100) return 1200;
+    if (litros >= 2101 && litros <= 3800) return 1200;
+    if (litros >= 3801) return 2500;
+    return "Altura del tinaco no disponible";
+}
+
 // Obtener los resultados almacenados en localStorage
 const resultados = JSON.parse(localStorage.getItem("resultados"));
 
 if (resultados) {
     document.getElementById("codigoPostal").innerText = resultados.ubicacion;
-    document.getElementById("zona").innerText = resultados.zona; // Mostrar la zona en los resultados
+    document.getElementById("zona").innerText = resultados.zona;
     document.getElementById("areaCaptacion").innerText = resultados.areaCaptacion;
     document.getElementById("niveles").innerText = resultados.niveles;
 
-    const capacidadTanque = 750;
+    const precipSem = 850;
     const metrosDistribucion = 1.5;
     const soporteTinaco = 0.5;
     const desperdicio = 1.05;
     const cantidadFiltro = 1;
     const reductor = 1;
+    const litros = 2000;
 
+    const capacidadTanque = obtenerTinaco(litros)
     const cantidadTuberia = calcularCantidadTuberia(resultados.niveles, capacidadTanque, metrosDistribucion, soporteTinaco, desperdicio);
 
-    document.getElementById("precipAnual").innerText = datosFijos.precipAnual;
+    document.getElementById("precipSem").innerText = `${precipSem} mm`;
     document.getElementById("capacidadTanque").innerText = `${capacidadTanque} L`;
     document.getElementById("materialTanque").innerText = obtenerMaterialRecomendado(capacidadTanque);
     document.getElementById("precioTanque").innerText = obtenerPrecioTanque(capacidadTanque);
